a6e2b6a4f39d05a76aaebaddb412bbff3b06b247
Gabrielli Ferreira <gabriellirodriguesferreira@gmail.com>
2024-10-09 16:33:36 -0300

    Composição

---
+++ Pessoa.java
@@ -0,0 +1,25 @@
+package Composicao;
+
+public class Pessoa {
+
+	private String nome;
+	private Coracao coracao; //Composição
+	
+	//Construtor
+	public Pessoa(String nome, int coracao) {
+		this.nome = nome;
+		this.coracao = new Coracao(coracao);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getCoracao() {
+		return coracao.getBatimentos();
+	}
+}
